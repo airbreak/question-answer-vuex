9d80d40b01ffcad8d6bd2c0d33be7d3cf3faec94
蒋建明 <jiangjianming@bmkp.cn>
2018-04-27 10:16:15 +0800

    finish basictly

--- src/components/itemcontainer.vue
+++ src/components/itemcontainer.vue
@@ -15,8 +15,8 @@
             <header class="item-title">{{itemDetail[itemNum - 1].topic_name}}</header>
             <ul>
               <li v-for="(item, index) in itemDetail[itemNum - 1].topic_answer"
-                  @click="choosed(item.id, item.topic_answer_id)" class="item-list">
-                    <span class="option-style" :class="{'has_choosed':chooseNum==index}">
+                  @click="choosed(index, item.topic_answer_id)" class="item-list">
+                    <span class="option-style" :class="{'has-choosed':chooseNum==index}">
                       {{chooseType(index)}}
                     </span>
                     <span class="option-detail">{{item.answer_name}}</span>
@@ -25,7 +25,7 @@
           </div>
         </div>
         <span class="next-item button-style" @click="nextItem" v-if="itemNum < itemDetail.length"></span>
-        <span class="sumit-item button-style" v-else @click="submitAnswer"></span>
+        <span class="submit-item button-style" v-else @click="submitAnswer"></span>
       </div>
     </section>
 </template>
@@ -74,8 +74,8 @@ export default {
       }
     },
     // 选中的答案信息
-    choosed (type, id) {
-      this.chooseNum = type
+    choosed (index, id) {
+      this.chooseNum = index
       this.choosedId = id
     },
     // 到达最后一题，交卷，清空定时器，跳转分数页面
@@ -91,7 +91,7 @@ export default {
   },
   created () {
     // 初始化信息
-    if (this.fatherComponent === 'home') {
+    if (this.component === 'home') {
       this.initializeData()
       document.body.style.backgroundImage = 'url(./static/img/1-1.jpg)'
     }
@@ -100,5 +100,107 @@ export default {
 </script>
 
 <style lang="less" scoped>
-
+  .top-tips{
+    position: absolute;
+    height: 7.35rem;
+    width: 3.25rem;
+    top: -1.3rem;
+    right: 1.6rem;
+    background: url(../images/WechatIMG2.png) no-repeat;
+    background-size: 100% 100%;
+    z-index: 10;
+    .num-tip{
+      position: absolute;
+      left: 0.48rem;
+      bottom: 1.1rem;
+      height: 0.7rem;
+      width: 2.5rem;
+      font-size: 0.6rem;
+      font-family: '黑体';
+      font-weight: 600;
+      color: #a57c50;
+      text-align: center;
+    }
+  }
+  .item-container-style{
+    height: 11.625rem;
+    width: 13.15rem;
+    background-repeat: no-repeat;
+    position: absolute;
+    top: 4.1rem;
+    left: 1rem;
+  }
+  .home-logo{
+    background-image: url(../images/1-2.png);
+    background-size: 13.142rem 100%;
+    background-position: right center;
+  }
+  .item-back{
+    background-image: url(../images/2-1.png);
+    background-size: 100% 100%;
+  }
+  .button-style{
+    display: block;
+    height: 2.1rem;
+    width: 4.35rem;
+    background-size: 100% 100%;
+    position: absolute;
+    top: 16.5rem;
+    left: 50%;
+    margin-left: -2.4rem;
+    background-repeat: no-repeat;
+  }
+  .start{
+    background-image: url(../images/1-4.png);
+  }
+  .next-item{
+    background-image: url(../images/2-2.png);
+  }
+  .submit-item{
+    background-image: url(../images/3-1.png);
+  }
+  .item-list-container{
+    position: absolute;
+    height: 7.0rem;
+    width: 8.0rem;
+    top: 2.4rem;
+    left: 3rem;
+    -webkit-font-smoothing: antialiased;
+  }
+  .item-title{
+    font-size: 0.65rem;
+    color: #fff;
+    line-height: 0.7rem;
+  }
+  .item-list{
+    font-size: 0;
+    margin-top: 0.4rem;
+    width: 10rem;
+    span{
+      display: inline-block;
+      font-size: 0.6rem;
+      color: #fff;
+      vertical-align: middle;
+    }
+    .option-style{
+      height: 0.725rem;
+      width: 0.725rem;
+      border: 1px solid #fff;
+      border-radius: 50%;
+      line-height: 0.725rem;
+      text-align: center;
+      margin-right: 0.3rem;
+      font-size: 0.5rem;
+      font-family: 'Arial';
+    }
+    .has-choosed{
+      background-color: #ffd400;
+      color: #575757;
+      border-color: #ffd400;
+    }
+    .option-detail{
+      width: 7.5rem;
+      padding-top: 0.11rem;
+    }
+  }
 </style>
--- src/page/score/index.vue
+++ src/page/score/index.vue
@@ -9,7 +9,7 @@
     <div class="share-button" @click="showCover"></div>
     <div class="share-code">
       <header class="share-header">关注我们，获取答案</header>
-      <img src="../../images/4-4.png" height="212" width="212" class="code_img"/>
+      <img src="../../images/4-4.png" height="212" width="212" class="code-img"/>
     </div>
     <div class="share-cover" v-show="showHide" @click="showCover">
       <img src="../../images/5-2.png" class="share-img"/>
@@ -41,7 +41,7 @@ export default {
       })
     },
     showCover () {
-      this.showHidden = !this.showHidden
+      this.showHide = !this.showHide
     },
     // 根据分数提示
     getScoreTip () {
@@ -49,22 +49,22 @@ export default {
         return this.scoreTipsArr[0]
       }
       if (this.score <= 40) {
-        return this.score[1]
+        return this.scoreTipsArr[1]
       }
       if (this.score <= 60) {
-        return this.score[2]
+        return this.scoreTipsArr[2]
       }
       if (this.score <= 80) {
-        return this.score[3]
+        return this.scoreTipsArr[3]
       }
       if (this.score <= 100) {
-        return this.score[4]
+        return this.scoreTipsArr[4]
       }
     }
   },
   created () {
     this.computedScore()
-    this.getScoreTip()
+    this.scoreTips = this.getScoreTip()
     document.body.style.backgroundImage = 'url(./static/img/4-1.jpg)'
   }
 }
@@ -75,79 +75,79 @@ export default {
     background-image: url(../../images/4-1.jpg);
     padding-top: 1.2rem;
   }
-  /*.your_scores_container{*/
-    /*width: 9.7rem;*/
-    /*height: 9.1rem;*/
-    /*background: url(../../images/4-2.png) no-repeat;*/
-    /*background-size: 100% 100%;*/
-    /*margin: 0 auto 0;*/
-    /*position: relative;*/
-    /*.your_scores{*/
-      /*position: absolute;*/
-      /*width: 100%;*/
-      /*text-indent: 3.3rem;*/
-      /*top: 4.7rem;*/
-      /*font-size: 1.4rem;*/
-      /*font-weight: 900;*/
-      /*-webkit-text-stroke: 0.05rem #412318;*/
-      /*font-family: 'Microsoft YaHei';*/
-      /*.score_num{*/
-        /*font-family: Tahoma,Helvetica,Arial;*/
-        /*color: #a51d31;*/
-      /*}*/
-      /*.fenshu{*/
-        /*color: #a51d31;*/
-      /*}*/
-    /*}*/
-    /*.result_tip{*/
-      /*position: absolute;*/
-      /*top: 7rem;*/
-      /*width: 9rem;*/
-      /*left: 0.6rem;*/
-      /*color: #3e2415;*/
-      /*font-size: 0.65rem;*/
-      /*text-align: center;*/
-    /*}*/
-  /*}*/
-  /*.share_button{*/
-    /*width: 6.025rem;*/
-    /*height: 2.4rem;*/
-    /*margin: 0.8rem auto 0;*/
-    /*background: url(../../images/4-3.png) no-repeat 0.4rem 0;*/
-    /*background-size: 5.825rem 100%;*/
-  /*}*/
-  /*.share_code{*/
-    /*width: 5.3rem;*/
-    /*margin: 1.5rem auto 0;*/
-    /*.share_header{*/
-      /*color: #664718;*/
-      /*font-size: 0.475rem;*/
-      /*font-family: 'Microsoft YaHei';*/
-      /*width: 7rem;*/
-      /*font-weight: 500;*/
-    /*}*/
-    /*.code_img{*/
-      /*height: 5.3rem;*/
-      /*width: 5.3rem;*/
-      /*margin-top: 0.5rem;*/
-    /*}*/
-  /*}*/
-  /*.share_cover{*/
-    /*position: fixed;*/
-    /*bottom: 0;*/
-    /*right: 0;*/
-    /*top: 0;*/
-    /*left: 0;*/
-    /*background: url(../../images/5-1.png) no-repeat;*/
-    /*background-size: 100% 100%;*/
-    /*opacity: 0.92;*/
-  /*}*/
-  /*.share_img{*/
-    /*height: 10.975rem;*/
-    /*width: 11.95rem;*/
-    /*position: fixed;*/
-    /*top: 0.5rem;*/
-    /*left: 50%;*/
-    /*margin-left: -5.975rem;*/
-  /*}*/
+  .your-scores-container{
+    width: 9.7rem;
+    height: 9.1rem;
+    background: url(../../images/4-2.png) no-repeat;
+    background-size: 100% 100%;
+    margin: 0 auto 0;
+    position: relative;
+    .your-scores{
+      position: absolute;
+      width: 100%;
+      text-indent: 3.3rem;
+      top: 4.7rem;
+      font-size: 1.4rem;
+      font-weight: 900;
+      -webkit-text-stroke: 0.05rem #412318;
+      font-family: 'Microsoft YaHei';
+      .score-num{
+        font-family: Tahoma,Helvetica,Arial;
+        color: #a51d31;
+      }
+      .fenshu{
+        color: #a51d31;
+      }
+    }
+    .result-tip{
+      position: absolute;
+      top: 7rem;
+      width: 9rem;
+      left: 0.6rem;
+      color: #3e2415;
+      font-size: 0.65rem;
+      text-align: center;
+    }
+  }
+  .share-button{
+    width: 6.025rem;
+    height: 2.4rem;
+    margin: 0.8rem auto 0;
+    background: url(../../images/4-3.png) no-repeat 0.4rem 0;
+    background-size: 5.825rem 100%;
+  }
+  .share-code{
+    width: 5.3rem;
+    margin: 1.5rem auto 0;
+    .share-header{
+      color: #664718;
+      font-size: 0.475rem;
+      font-family: 'Microsoft YaHei';
+      width: 7rem;
+      font-weight: 500;
+    }
+    .code-img{
+      height: 5.3rem;
+      width: 5.3rem;
+      margin-top: 0.5rem;
+    }
+  }
+  .share-cover{
+    position: fixed;
+    bottom: 0;
+    right: 0;
+    top: 0;
+    left: 0;
+    background: url(../../images/5-1.png) no-repeat;
+    background-size: 100% 100%;
+    opacity: 0.92;
+  }
+  .share-img{
+    height: 10.975rem;
+    width: 11.95rem;
+    position: fixed;
+    top: 0.5rem;
+    left: 50%;
+    margin-left: -5.975rem;
+  }
 </style>
